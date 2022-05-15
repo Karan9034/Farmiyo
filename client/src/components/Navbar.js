8fd57b7aeab7976ea7197a4efe9797e0f206d7b9
Hemant Bajaj <41hemu38@gmail.com>
2022-05-15 18:51:25 +0530

    feat: add navbar

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "../styles/Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="nav">
+      <div className="logo">
+        <h2>Cropify</h2>
+      </div>
+      <div className="navItems">
+        <h2>
+          <a href="/">Home</a>
+        </h2>
+        <h2>
+          <a href="/about">About</a>
+        </h2>
+        <h2>
+          <a href="/analyze">Analyze</a>
+        </h2>
+        <h2>
+          <a href="/shop">Shop</a>
+        </h2>
+      </div>
+      <div className="loginSection">
+        <div>
+          <button className="Language">Translate</button>
+          <button className="login">Login</button>
+        </div>
+        <div>
+          Don’t have an account? <a href="/signup">Signup here</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
